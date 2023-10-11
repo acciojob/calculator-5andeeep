2998d99b5ca4c341a12b30a03768e022d86315fc
Sandeep <newsandeep03@gmail.com>
2023-10-11 13:49:07 +0530

    ans added

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+
+const input = document.getElementById('input');
+const buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'C'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == '←'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
+})
